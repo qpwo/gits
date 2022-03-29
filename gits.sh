763a37b0a605521c014cacdfad2dfb68138d78f3
Luke Harold Miles <lukem@sent.com>
2022-03-28 18:00:18 -0700

    add a bunch more commands

--- gits.sh
+++ gits.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 set -euo pipefail
 
 function undo-commit() {
@@ -15,7 +17,7 @@ function ammend-message() {
 }
 
 function rename-branch() {
-    old=$(git branch)
+    old=$(git branch --show-current)
     git branch -M $1
     git push origin -u $1
     git push origin --delete $old
@@ -40,7 +42,7 @@ function discard-everything() {
 
 function checkout() {
     git fetch origin
-    git checkout $1
+    git switch $1
 }
 
 function untrack() {
@@ -61,4 +63,130 @@ function new-branch() {
 
 function push-all-branches() {
     git push --all -u
+}
+
+function add-empty-dir() {
+    echo '*' >>$1/.gitignore
+    echo '!.gitignore' >>$1/.gitignore
+    git add $1
+}
+
+function combine-last-n-commits() {
+    git stash
+    git reset --hard HEAD~$1
+    git merge --squash HEAD@{1}
+    git commit
+    git push -f
+}
+
+function back-to-last-pushed() {
+    git reset --hard origin/$(git branch --show-current)
+}
+
+function delete-remote-tag() {
+    git push --delete origin $1
+}
+
+function change-url() {
+    git remote set-url origin $1
+}
+
+function undo-rebase() {
+    git reset --hard ORIG_HEAD
+}
+
+function move-working-tree-to-branch() {
+    git switch -c $1
+}
+
+function file-history() {
+    git log -p -- $1
+}
+
+function current-branch() {
+    git branch --show-current
+}
+
+function files-in-commit() {
+    git diff-tree --no-commit-id --name-only -r $1
+}
+
+function undo-hard-reset() {
+    git reset 'HEAD@{1}'
+}
+
+function set-commit-editor() {
+    git config --global core.editor
+}
+
+function push-tag() {
+    git push origin $1
+}
+
+function push-all-tags() {
+    git push --follow-tags
+}
+
+function stash-interactive() {
+    git stash push -p -m $1
+}
+
+function apply-commit() {
+    git cherry-pick -x $1
+}
+
+function export() {
+    git archive --format zip --output export.zip main
+    echo export.zip
+}
+
+function show-staged() {
+    git diff --staged
+}
+
+function last-commit-hash() {
+    git rev-parse --short HEAD
+}
+
+function delete-all-merged-branches() {
+    git branch --merged | egrep -v "(^\*|master|main)" | xargs git branch -d
+    git remote prune origin
+    git push
+}
+
+function branch-from-n-before() {
+    git checkout -b $1 HEAD~$2
+}
+
+function ignore-except() {
+    echo '*' >>.gitignore
+    for var in "$@"; do
+        echo "!$var" >>.gitignore
+    done
+}
+
+function update-all-submodules() {
+    git submodule update --recursive --remote
+}
+
+function unpushed-commits() {
+    git log origin/$(git branch --show-current)..HEAD
+}
+
+function unpushed-commits-diff() {
+    git diff origin/$(git branch --show-current)..HEAD
+}
+
+function clone-at-tag() {
+    git clone -b $2 --single-branch --depth 1 $1
+}
+
+function clone-shallow() {
+    git clone $1 --depth 1
+}
+
+function pull-all-branches() {
+    git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+    git fetch --all
+    git pull --all
 }
